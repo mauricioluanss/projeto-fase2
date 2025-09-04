33b733f1736d46f2c1f6f5d426ef3cd9243199ea
Mauricio Luan <mauricioluan2023@gmail.com>
2025-09-04 14:37:37 -0300

    feat: insere try..except na cria_dicionario e explicita tipos dos retornos das funcoes

--- main.py
+++ main.py
@@ -12,45 +12,55 @@ def cria_matriz(arquivo) -> list[str]:
 
 
 # retorno so a linha de cabeçalho da matriz
-def cabecalho_matriz(matriz):
+def cabecalho_matriz(matriz) -> list[str]:
     return matriz[0]
 
 
 # retorno todas as linhas do corpo da matriz, exceto a primeira, qu é o cabeçalho
-def corpo_matriz(matriz):
+def corpo_matriz(matriz) -> list[list[str]]:
     return matriz[1:]
 
 
-# eu preciso fazer com que cada linha da matriz vire um dicionario, onde a chave é o nome da coluna e o valor é o elemento
-def cria_dicionario(coluna, linhas):
+# retorna uma lista de dicionarios, onde cada elemento representa uma linha do csv original.
+# as chaves são os nomes das colunas e os valores são os valores das colunas.
+def cria_dicionario(
+    coluna: list[str], linhas: list[list[str]]
+) -> list[dict[str, str | float]]:
     dados_meteorologicos = []
-    for linha in linhas:
-        dados_meteorologicos.append(
-            {
-                coluna[0]: linha[0],  # "data": "01/01/1961",
-                coluna[1]: linha[1],  # "precip": "8.6" <-ja converto pra float aqui?
-                coluna[2]: linha[2],  # ...
-                coluna[3]: linha[3],
-                coluna[4]: linha[4],
-                coluna[5]: linha[5],
-                coluna[6]: linha[6],
-                coluna[7]: linha[7],
-            }
-        )
-    # criei este aquivo só pra ver como os dados estão sendo salvos
-    # APAGAR DEPOIS KKK
-    with open("dicionario.txt", "w") as arquivo:
-        arquivo.write(str(dados_meteorologicos))
-    print(dados_meteorologicos)
+    try:
+        for elemento in linhas:
+            dados_meteorologicos.append(
+                {
+                    coluna[0]: elemento[0],  # "data": "01/01/1961",
+                    coluna[1]: float(elemento[1]),  # "precip": 8.6
+                    coluna[2]: float(elemento[2]),  # ...
+                    coluna[3]: float(elemento[3]),
+                    coluna[4]: float(elemento[4]),
+                    coluna[5]: float(elemento[5]),
+                    coluna[6]: float(elemento[6]),
+                    coluna[7]: float(elemento[7]),
+                }
+            )
+        return dados_meteorologicos
+    except IndexError as indexError:
+        print(f"Erro: Coluna/linha com número insuficiente de elementos: {indexError}")
+    except TypeError as typeError:
+        print(f"Erro: tipo de dado inválido em coluna ou linha: {typeError}")
+    except ValueError as valueError:
+        print(f"Erro: valor inválido na conversão para float: {valueError}")
+        # exit()? <- devo colocar exit em cada execpet? nao quero que o prgrama continue se tiver erro
 
 
 def main():
     caminho_arquivo = "dados_meteorologicos.csv"
     with open(caminho_arquivo, "r") as arquivo:
         matriz = cria_matriz(arquivo)
-        cabecalho = cabecalho_matriz(matriz)
-        corpo = corpo_matriz(matriz)
-        cria_dicionario(cabecalho, corpo)
+
+    cabecalho = cabecalho_matriz(matriz)
+    corpo = corpo_matriz(matriz)
+    dados_meteorologicos = cria_dicionario(cabecalho, corpo)
+
+    print(dados_meteorologicos)
 
 
 main()
